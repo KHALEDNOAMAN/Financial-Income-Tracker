9c626f1a2a7dca4fa02c797e44098cc22b670020
Khaled Noaman <81718367+KHALEDNOAMAN@users.noreply.github.com>
2026-06-23 10:51:34 +0300

    Add backend/src/routes/incomeRoutes.js

---
+++ backend/src/routes/incomeRoutes.js
@@ -0,0 +1,8 @@
+﻿const express = require('express');
+const router = express.Router();
+const IncomeController = require('../controllers/incomeController');
+router.get('/', IncomeController.getAll);
+router.get('/:id', IncomeController.getById);
+router.post('/', IncomeController.create);
+router.put('/:id', IncomeController.update);
+module.exports = router;
