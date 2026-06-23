abe4c4e8f9d75bb469408a9ecfad69a0461e524c
Khaled Noaman <81718367+KHALEDNOAMAN@users.noreply.github.com>
2026-06-23 10:51:31 +0300

    Add backend/src/routes/accountRoutes.js

---
+++ backend/src/routes/accountRoutes.js
@@ -0,0 +1,6 @@
+﻿const express = require('express');
+const router = express.Router();
+const AccountController = require('../controllers/accountController');
+router.get('/', AccountController.getAll);
+router.get('/:id', AccountController.getById);
+module.exports = router;
