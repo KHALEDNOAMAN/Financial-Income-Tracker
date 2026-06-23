a56a9975cc1ccb21d4f578646881e8228193d6ec
Khaled Noaman <81718367+KHALEDNOAMAN@users.noreply.github.com>
2026-06-23 10:51:32 +0300

    Add backend/src/routes/fiscalRoutes.js

---
+++ backend/src/routes/fiscalRoutes.js
@@ -0,0 +1,6 @@
+﻿const express = require('express');
+const router = express.Router();
+const FiscalController = require('../controllers/fiscalController');
+router.get('/', FiscalController.getAll);
+router.post('/:id/close', FiscalController.close);
+module.exports = router;
